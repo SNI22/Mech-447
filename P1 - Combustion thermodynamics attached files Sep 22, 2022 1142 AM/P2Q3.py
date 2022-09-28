a8d664f92f3d8898fc635111b7567f55b7fe83f0
SNI22 <105662478+SNI22@users.noreply.github.com>
2022-09-28 05:29:40 -0400

    Changes to be committed: 	new file:   P1 - Combustion thermodynamics attached files Sep 22, 2022 1142 AM/.ipynb_checkpoints/P1 - Combustion thermodynamics attached files Sep 22, 2022 1142 AM-checkpoint.code-workspace 	modified:   P1 - Combustion thermodynamics attached files Sep 22, 2022 1142 AM/.ipynb_checkpoints/ThermoPropertiesNASA-checkpoint.ipynb 	modified:   P1 - Combustion thermodynamics attached files Sep 22, 2022 1142 AM/P2Q3.py 	modified:   P1 - Combustion thermodynamics attached files Sep 22, 2022 1142 AM/P2Q4.py 	modified:   P1 - Combustion thermodynamics attached files Sep 22, 2022 1142 AM/ThermoPropertiesNASA.ipynb 	new file:   P1 - Combustion thermodynamics attached files Sep 22, 2022 1142 AM/__pycache__/P2Q3.cpython-310.pyc 	new file:   P1 - Combustion thermodynamics attached files Sep 22, 2022 1142 AM/__pycache__/P2Q3.cpython-39.pyc

--- P1 - Combustion thermodynamics attached files Sep 22, 2022 1142 AM/P2Q3.py
+++ P1 - Combustion thermodynamics attached files Sep 22, 2022 1142 AM/P2Q3.py
@@ -3,7 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 phi=np.arange(0.5,1.55,0.05)
-print(phi[20])
 Tf = np.empty(21)
 
 T_ref = 300 #K
@@ -12,10 +11,9 @@ Cp = 3.5 * R
 
 def h_gap_lean(phi):
     delta_h = - phi * thermo('C8H18',114).h_mole(T_ref) - 12.5 * thermo('O2',32).h_mole(T_ref) \
-    + 8 * phi * thermo('CO2',44).h_mole(T_ref) \
-    + 9 * phi * thermo('H2O',18).h_mole(T_ref) \
-    + (12.5 - 12.5 * phi) * thermo('O2',32).h_mole(T_ref)
-    
+        + 8 * phi * thermo('CO2',44).h_mole(T_ref) + 9 * phi * thermo('H2O',18).h_mole(T_ref) \
+        + (12.5 - 12.5 * phi) * thermo('O2',32).h_mole(T_ref) 
+        
     return delta_h
 
 def h_gap_rich(phi):
@@ -28,13 +26,12 @@ def h_gap_rich(phi):
     return delta_h
 
 
+def calculate_Tf():
+    for a in range (len(phi)):
+        if phi[a] == 1 or phi[a] < 1:
+            Tf[a] = (- h_gap_lean(phi[a])) /(Cp*(4.5*phi[a] + 47 + 12.5)) + T_ref
+        else:
+            Tf[a] = (- h_gap_rich(phi[a])) / (Cp*( - 15.2 * phi[a] + 8*phi[a] + 24.2*phi[a] + 47)) + T_ref
 
-for a in range (len(phi)):
-    if phi[a] == 1 or phi[a] < 1:
-        Tf[a] = (- h_gap_lean(phi[a])) /(Cp*(4.5*phi[a] + 47 + 12.5)) + 300
-    else:
-        Tf[a] = (- h_gap_rich(phi[a])) / (Cp*( - 15.2 * phi[a] + 8*phi[a] + 24.2*phi[a] + 47)) + 300
-
+calculate_Tf()
 print(Tf)
-
-
--- P1 - Combustion thermodynamics attached files Sep 22, 2022 1142 AM/P2Q4.py
+++ P1 - Combustion thermodynamics attached files Sep 22, 2022 1142 AM/P2Q4.py
@@ -1,2 +1,61 @@
-from P2Q3 import *
+from ipynb.fs.full.ThermoPropertiesNASA import thermo
+from P2Q3 import phi #h_gap_rich, R #h_gap_lean, R
+from scipy.integrate import quad 
+from scipy.optimize import fsolve as solve
+import numpy as np
 
+Tf_int = np.empty(21)
+T_ref = int(300)
+
+def Cp_lean(Tad):
+    return 8 * phi[a] * thermo('CO2',44).cp_mole(Tad) + 9 * phi[a] * thermo('H2O',18).cp_mole(Tad) \
+        + (12.5 - 12.5 * phi[a]) * thermo('O2',32).cp_mole(Tad) + 47 * thermo('N2',28).cp_mole(Tad)
+
+def h_int_lean(Tad):
+    #print(quad(Cp_lean(Tad), T_ref, Tad)[0])
+    return quad(Cp_lean, T_ref, Tad)[0]
+
+def h_gap_lean():
+    x = 0.0
+    x = phi[a]
+    delta_h = - x * thermo('C8H18',114).h_mole(T_ref) - 12.5 * thermo('O2',32).h_mole(T_ref) \
+        + 8 * x * thermo('CO2',44).h_mole(T_ref) + 9 * x * thermo('H2O',18).h_mole(T_ref) \
+        + (12.5 - 12.5 * x) * thermo('O2',32).h_mole(T_ref) 
+    return delta_h
+
+def lean_gap(Tad):
+    return h_gap_lean() + h_int_lean(Tad)
+
+
+def Cp_rich(Tad):
+    Cp = 0.8 * phi[a] * thermo('CO',28).cp_mole(Tad) \
+    + 7.2 * phi[a] * thermo('CO2',44).cp_mole(Tad) \
+    + (25-15.2*phi[a]) * thermo('H2O',18).cp_mole(Tad) \
+    + (24.2 * phi[a] - 25) * thermo('H2',2).cp_mole(Tad) + 47 * thermo('N2',28).cp_mole(Tad)
+
+    return Cp
+
+def h_int_rich(Tad):
+    return quad(Cp_rich, T_ref, Tad)[0]
+
+def h_gap_rich():
+    delta_h = - phi[a] * thermo('C8H18',114).h_mole(T_ref) - 12.5 * thermo('O2',32).h_mole(T_ref) \
+    + 0.8 * phi[a] * thermo('CO',28).h_mole(T_ref) \
+    + 7.2 * phi[a] * thermo('CO2',44).h_mole(T_ref) \
+    + (25-15.2*phi[a]) * thermo('H2O',18).h_mole(T_ref) \
+    + (24.2 * phi[a] - 25) * thermo('H2',2).h_mole(T_ref) 
+    return delta_h
+
+def rich_gap(Tad):
+    return h_gap_rich() + h_int_rich(Tad)
+
+    
+
+for a in range(len(phi)):
+    if phi[a] == 1 or phi[a] < 1:
+        Tf_int[a] = solve(lean_gap, 1000)
+        print(Tf_int[a])
+    else:
+        Tf_int[a] = solve(rich_gap, 1000)
+        print(Tf_int[a])
+print(Tf_int)
